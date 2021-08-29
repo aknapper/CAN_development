46d5d10adee6d8d638c85c2e65ab9902b3bb8255
aknapper <34011307+aknapper@users.noreply.github.com>
2021-08-28 18:59:43 -0700

    update dbc file location

--- tools/LoadDBC.py
+++ tools/LoadDBC.py
@@ -1,7 +1,7 @@
 import cantools
 
 from can.message import Message
-db = cantools.db.load_file('/home/aaron/workspace/CAN_dev/toyota_camry_hybrid_2018_pt_generated.dbc')
+db = cantools.db.load_file('/home/aaron/workspace/CAN_development/toyota_camry_hybrid_2018_pt_generated.dbc')
 print(db)
 
 msg = db.get_message_by_name('EPS_STATUS')
--- tools/SendCANDBCMessage.py
+++ tools/SendCANDBCMessage.py
@@ -3,7 +3,7 @@ import can
 
 from can.message import Message
 from cantools.database.can import attribute
-db = cantools.db.load_file('/home/aaron/workspace/CAN_dev/toyota_camry_hybrid_2018_pt_generated.dbc')
+db = cantools.db.load_file('/home/aaron/workspace/CAN_development/toyota_camry_hybrid_2018_pt_generated.dbc')
 
 msg = db.get_message_by_name('BRAKE_MODULE')
 msg_data = msg.encode({'BRAKE_PRESSURE':1,'BRAKE_POSITION':1,'BRAKE_PRESSED':1})
